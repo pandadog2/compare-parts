d68a389a17864ab0ecdadf18a06e7b4e67aa3a54
emiumiu1 <emiumiu1@gmail.com>
2021-08-08 00:05:23 +0900

    検索結果では1枚目のimageのみ表示する

--- app/models/part.rb
+++ app/models/part.rb
@@ -17,33 +17,5 @@ class Part < ApplicationRecord
 
   validates :product_number, format: { with: /\A[0-9]{5}-[0-9]{5}\z/, message: "is 5 digits-5 digits (half-width number)" }, uniqueness: { case_sensitive: true }
   validates :thickness, :weight, numericality: true, format: { with: /\A[0-9]{0,6}\.[0-9]{2}\z/, message: "is 2 digits for the decimal part and 6 digits or less for the natural number part" }
-
-  # validates :image_type, :image_size, :image_length
-
-  # private
-
-  # def image_type
-  #   images.each do |image|
-  #     if !image.blob.content_type.in?(%('image/jpeg image/png'))
-  #       image.purge
-  #       errors.add(images, "はjpegまたはpng形式でアップロードしてください")
-  #     end
-  #   end
-  # end
-
-  # def image_size
-  #   images.each do |image|
-  #     if image.blob.byte_size > 1.megabytes
-  #       avatar.purge
-  #       errors.add(:images, "は1つのファイルで5MB以内にしてください")
-  #     end
-  #   end
-  # end
-
-  # def image_length
-  #   if images.length > 5
-  #     images.purge
-  #     errors.add(:images, "は5枚以内にしてください")
-  #   end
-  # end
+  validates :images, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..2.megabytes }
 end
