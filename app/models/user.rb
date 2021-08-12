30a37cf43126cb9d25dfd55b5ee69f284a6c7688
emiumiu1 <emiumiu1@gmail.com>
2021-08-12 20:32:15 +0900

    半角スペース入力可能に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   has_many :car_models
   has_many :parts
        
-  validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ーa-zA-Z]+\z/, message: "is other than numbers and double-byte alphabets" }, presence: true, length: { maximum: 10 }
+  validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ーa-zA-Z +]+\z/, message: "is other than numbers, double-byte alphabets, and double-byte spaces" }, presence: true, length: { maximum: 10 }
 end
