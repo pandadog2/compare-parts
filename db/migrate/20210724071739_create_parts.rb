7bead8965738d44cfba537235ae07aab9e773c7d
emiumiu1 <emiumiu1@gmail.com>
2021-08-18 20:55:48 +0900

    承認日を追加

--- db/migrate/20210724071739_create_parts.rb
+++ db/migrate/20210724071739_create_parts.rb
@@ -7,6 +7,7 @@ class CreateParts < ActiveRecord::Migration[6.0]
       t.string     :thickness,      null: false
       t.string     :weight,         null: false
       t.integer    :supplier_id,    null: false
+      t.date       :approval_date,  null: false
       t.references :user,           null: false, foreign_key: true
       t.references :car_model,      null: false, foreign_key: true
       t.timestamps
